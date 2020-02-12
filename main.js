db64a9a3a771670b8413618ec618fae18dfc515e
Kat <kmneil2@gmail.com>
2020-02-12 18:45:42 -0500

    Set canvas and add player

---
+++ main.js
@@ -0,0 +1,52 @@
+class Game {
+  constructor () {
+    const canvas = document.querySelector('#run')
+    const screen = canvas.getContext('2d')
+    const gameSize = { x: canvas.width, y: canvas.height }
+    this.player = new Player(this, gameSize)
+
+    console.log(this.player)
+
+    const tick = () => {
+      this.update()
+      this.draw(screen, gameSize)
+      requestAnimationFrame(tick)
+    }
+    tick()
+  }
+
+  update () {
+    this.player.update()
+    console.log('update')
+  }
+
+  draw (screen, gameSize) {
+    screen.clearRect(0, 0, gameSize.x, gameSize.y)
+    drawRect(screen, this.player)
+  }
+}
+
+class Player {
+  constructor (game, gameSize) {
+    this.game = game
+    this.size = { x: 12, y: 12 }
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
+    // this.center = { x: 50, y: 50 }
+    this.keyboarder = Keyboarder
+  }
+  
+  update () {
+    if (this.keyboarder.isDown(this.keyboarder.keyState.LEFT)) {
+      this.center.x -= 2
+    } else if (this.keyboarder.isDown((this.keyboarder.keyState.RIGHT))) {
+      this.center.x += 2
+    }
+  }
+}
+
+function drawRect (screen, body) {
+  screen.fillRect(body.center.x - body.size.x / 2, body.center.y / 2, body.size.x, body.size.y)
+}
+window.addEventListener('load', function () {
+  new Game()
+})
